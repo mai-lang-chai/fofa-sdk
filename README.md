0751499ed26400942abde8b834242e61ccc82658
H@cker❤P@inter <36095584+mai-lang-chai@users.noreply.github.com>
2019-11-18 11:33:10 +0800

    fofa-sdk 1.0
    1.0

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # fofa-sdk
 FOFA sdk批量导出ip,port,title,protocol
+
+
+![](https://img.shields.io/badge/fofa-sdk-blueviolet)
+![](https://img.shields.io/badge/Python-2.7-orange)
+
+
+
+Tips:  
+	一页100条数据，不定期更新写入excel、多线程、GPU、多核版本
+
+![image](/PIC/r.jpg)
+
+useage：
+
+```
+python fofa-sdk.py -m 123456@gmail.com -k e839b8781c6b60d381562823e1123456 -q /login.rsp -p 3 -o 2018-9995.txt"
+```
---
+++ fofa-sdk.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import fofa,argparse,os
+
+banner = '''
+    _/_/_/_/    _/_/    _/_/_/_/    _/_/    
+   _/        _/    _/  _/        _/    _/   
+  _/_/_/    _/    _/  _/_/_/    _/_/_/_/    
+ _/        _/    _/  _/        _/    _/     
+_/          _/_/    _/        _/    _/      
+     by https://github.com/mai-lang-chai
+        '''
+print banner
+
+def Request(m,k,p,q):
+    email, key = (m,k)
+    client = fofa.Client(email, key)
+    global Sum
+    query_str = q
+    Sum = []
+    for page in range(int(p)-1):
+        data = client.get_data(query_str, fields="ip,port,title,protocol" ,page=page)
+        Sum.append(data)
+
+def save(Sum,o):
+    for l in Sum:
+        for i in range(len(l["results"])):
+            ip = l["results"][i][0]
+            port = l["results"][i][1]
+            title = l["results"][i][2]
+            protocol = l["results"][i][3]
+            try:
+                url = "http://" + ip + ":" + port
+                with open(o,'a+') as f:
+                    f.write(url+"--"+title+"--"+protocol+"--"+'\n')
+            except:
+                pass
+    print "File save as  " + os.getcwd() + "\\" + o
+
+if __name__ == '__main__':
+    parser =argparse.ArgumentParser(description="python fofa-sdk.py -m 123456@gmail.com -k e839b8781c6b60d381562823e1123456 -q /login.rsp -p 3 -o 2018-9995.txt" )
+    parser.add_argument('-m', '--mail', default='', help="123456@gmail.com")
+    parser.add_argument('-k', '--key', default='', help="e839b878137160d381562823e13c6e02")
+    parser.add_argument('-q', '--query', default='', help="/login.rsp")
+    parser.add_argument('-p', '--page', default='', help="5")
+    parser.add_argument('-o', '--output', default='A.txt', help="CVE-2018-9995.txt")
+    args = parser.parse_args()
+    m = ''
+    k = ''
+    q = ''
+    p = ''
+    o = ''
+    if args.mail:
+        m = args.mail
+    if args.key:
+        k = args.key
+    if args.query:
+        q = args.query
+    if args.page:
+        p = args.page
+    if args.output:
+        o = args.output
+    Request(m,k,p,q)
+    save(Sum,o)
